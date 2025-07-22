0b4939fcb3d675a2647d8324b9065bc967e62b8c
Idyllac <victor.via7@gmail.com>
2025-07-22 20:38:45 +0200

    changes

--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,4 @@
-// auth.js
+// routes/auth.js
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config();
 }
--- routes/public.js
+++ routes/public.js
@@ -24,15 +24,4 @@ router.post('/login', checkNotAuthenticated, passport.authenticate('local', {
 
 router.delete('/logout', authController.logout);
 
-
-// // GET /register - serve register page
-// router.get('/register', (req, res) => {
-//   res.render('register'); // assumes views/register.ejs exists
-// });
-
-// // GET /login - serve login page
-// router.get('/login', (req, res) => {
-//   res.render('login'); // assumes views/login.ejs exists
-// });
-
 module.exports = router;
