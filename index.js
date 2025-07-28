1ff909e1ed8941cf8a4b3b7d58b61dfd32fb707a
Idyllac <victor.via7@gmail.com>
2025-07-29 00:47:52 +0200

    clear changes

--- index.js
+++ index.js
@@ -16,8 +16,6 @@ const methodOverride = require('method-override');
 const cors = require('cors');
 const SECRET = process.env.JWT_SECRET || 'your_jwt_secret';
 const REFRESH_SECRET = process.env.JWT_REFRESH_SECRET || 'your_refresh_secret';
-// const testRoutes = require('./routes/test');
-
 
 
 
