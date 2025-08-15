a9bd2999dcf8d132afd9b40bb286d21b246d1d77
Idyllac <victor.via7@gmail.com>
2025-08-15 19:38:51 +0200

    changes

--- config/passport-jwt.js
+++ config/passport-jwt.js
@@ -2,7 +2,7 @@
 // const { Strategy: JwtStrategy, ExtractJwt } = require('passport-jwt');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
-const { User } = require('../models/User'); // Adjust path based on your structure
+const { User } = require('../models'); // Adjust path based on your structure
 
 // Options for extracting JWT from Authorization header as Bearer token
 const opts = {
