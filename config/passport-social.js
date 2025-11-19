d4830ff9b89c80c150ebe9e66ff718d3404b4fbc
Idyllac <victor.via7@gmail.com>
2025-11-19 21:11:05 +0100

    providers changes

--- config/passport-social.js
+++ config/passport-social.js
@@ -6,10 +6,10 @@ function configureSocialStrategies(passport) {
   require('./passport-google')(passport);
   require('./passport-github')(passport);
   require('./passport-twitter')(passport);
-  require('./passport-instagram')(passport);
+  // require('./passport-instagram')(passport);
   require('./passport-linkedin')(passport);
   // require('./passport-youtube')(passport);
-  require('./passport-tiktok')(passport);
+  // require('./passport-tiktok')(passport);
 }
 
 // ✅ Export as an object (so passport.js can destructure it)
