8a3fb572e095423baad932c93db5ca19a3005527
Idyllac <victor.via7@gmail.com>
2025-11-22 00:52:53 +0100

    answers changes

--- config/passport-twitter.js
+++ config/passport-twitter.js
@@ -48,6 +48,86 @@ module.exports = (passport) => {
 
 
 
+// // config/passport-twitter.js
+
+// const TwitterStrategy = require("passport-twitter-oauth2").Strategy;
+// const { User } = require("../models");
+
+// module.exports = function (passport) {
+//   passport.use(
+//     new TwitterStrategy(
+//       {
+//         clientID: process.env.TWITTER_CLIENT_ID,
+//         clientSecret: process.env.TWITTER_CLIENT_SECRET,
+//         callbackURL: process.env.TWITTER_CALLBACK_URL,
+//         scope: ['tweet.read', 'users.read', 'offline.access'],
+//       },
+//       async function (accessToken, refreshToken, profile, done) {
+//         try {
+//           let user = await User.findOne({ where: { twitterId: profile.id } });
+
+//           if (!user) {
+//             user = await User.create({
+//               twitterId: profile.id,
+//               name: profile.displayName,
+//               email: profile.emails?.[0]?.value || null,
+//             });
+//           }
+
+//           return done(null, user);
+//         } catch (err) {
+//           console.error("Twitter OAuth error:", err);
+//           return done(err, null);
+//         }
+//       }
+//     )
+//   );
+// };
+
+
+
+
+
+
+
+// import { Strategy as TwitterStrategy } from "passport-twitter-oauth2";
+// import passport from "passport";
+// import User from "../models/User.js";
+
+// passport.use(
+//   new TwitterStrategy(
+//     {
+//       clientID: process.env.TWITTER_CLIENT_ID,
+//       clientSecret: process.env.TWITTER_CLIENT_SECRET,
+//       callbackURL: "https://anypay.cards/auth/twitter/callback",
+//       scope: ["tweet.read", "users.read", "offline.access"],
+//     },
+//     async (accessToken, refreshToken, profile, done) => {
+//       try {
+//         let user = await User.findOne({ where: { twitterId: profile.id } });
+
+//         if (!user) {
+//           user = await User.create({
+//             twitterId: profile.id,
+//             username: profile.username,
+//           });
+//         }
+
+//         return done(null, user);
+//       } catch (err) {
+//         return done(err, null);
+//       }
+//     }
+//   )
+// );
+
+// export default passport;
+
+
+
+
+
+
 
 
 
