6657c14e49fc494464003261597ad56a929b0e85
Idyllac <victor.via7@gmail.com>
2025-12-15 02:25:16 +0100

    final changes

--- index.js
+++ index.js
@@ -247,14 +247,31 @@ app.get('/default', (req, res) => res.sendFile(path.join(__dirname, 'public', 'i
 app.get('/ar', (req, res) => res.sendFile(path.join(__dirname, 'public', 'indexAr.html')));
 app.get('/fr', (req, res) => res.sendFile(path.join(__dirname, 'public', 'indexFr.html')));
 app.get('/en', (req, res) => res.sendFile(path.join(__dirname, 'public', 'indexEn.html')));
-app.get('/subscribeAr', (req, res) => res.sendFile(path.join(__dirname, 'public', 'subscribeAr.html')));
+app.get('/subscribe', (req, res) => res.sendFile(path.join(__dirname, 'public', 'subscribe.html')));
 app.get('/subscribeEn', (req, res) => res.sendFile(path.join(__dirname,  'public', 'subscribeEn.html')));
 app.get('/subscribeFr', (req, res) => res.sendFile(path.join(__dirname, 'public', 'subscribeFr.html')));
 app.get('/local', (req, res) => res.sendFile(path.join(__dirname, 'public', 'local.html')));
+app.get('/localEn', (req, res) => res.sendFile(path.join(__dirname, 'public', 'localEn.html')));
+app.get('/localFr', (req, res) => res.sendFile(path.join(__dirname, 'public', 'localFr.html')));
 app.get('/international', (req, res) => res.sendFile(path.join(__dirname, 'public', 'international.html')));
+app.get('/internationalEn', (req, res) => res.sendFile(path.join(__dirname, 'public', 'internationalEn.html')));
+app.get('/internationalFr', (req, res) => res.sendFile(path.join(__dirname, 'public', 'internationalFr.html')));
 app.get('/about', (req, res) => res.sendFile(path.join(__dirname, 'public', 'about.html')));
+app.get('/aboutEn', (req, res) => res.sendFile(path.join(__dirname, 'public', 'aboutEn.html')));
+app.get('/aboutFr', (req, res) => res.sendFile(path.join(__dirname, 'public', 'aboutFr.html')));
 app.get('/contact', (req, res) => res.sendFile(path.join(__dirname, 'public', 'contact.html')));
+app.get('/contactEn', (req, res) => res.sendFile(path.join(__dirname, 'public', 'contactEn.html')));
+app.get('/contactFr', (req, res) => res.sendFile(path.join(__dirname, 'public', 'contactFr.html')));
 app.get('/hours', (req, res) => res.sendFile(path.join(__dirname, 'public', 'hours.html')));
+app.get('/hoursEn', (req, res) => res.sendFile(path.join(__dirname, 'public', 'hoursEn.html')));
+app.get('/hoursFr', (req, res) => res.sendFile(path.join(__dirname, 'public', 'hoursFr.html')));
+
+app.get('/privacy', (req, res) => res.sendFile(path.join(__dirname, 'public/legal', 'privacy.html')));
+app.get('/privacyEn', (req, res) => res.sendFile(path.join(__dirname, 'public/legal', 'privacyEn.html')));
+app.get('/privacyFr', (req, res) => res.sendFile(path.join(__dirname, 'public/legal', 'privacyFr.html')));
+app.get('/tos', (req, res) => res.sendFile(path.join(__dirname, 'public/legal', 'tos.html')));
+app.get('/tosEn', (req, res) => res.sendFile(path.join(__dirname, 'public/legal', 'tosEn.html')));
+app.get('/tosFr', (req, res) => res.sendFile(path.join(__dirname, 'public/legal', 'tosFr.html')));
 
 // Auth entry pages
 app.get('/login', checkNotAuthenticated, (req, res) => res.render('login'));
