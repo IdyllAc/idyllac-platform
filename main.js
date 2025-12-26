b6e5733694a12153f463f5563edd28405327f432
Idyllac <victor.via7@gmail.com>
2025-12-26 05:43:43 +0100

    final changes

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
- const year = document.getElementById("year")
- const thisYear = new Date().getFullYear()
- year. setAttribute ("datetime", thisYear)
- year.textContent = thisYear
+const year = document.getElementById("year")
+const thisYear = new Date().getFullYear()
+year. setAttribute ("datetime", thisYear)
+year.textContent = thisYear
 
- 
